4de14c056d17525dc25e5cbfc65dff99d07a0422
iphayao <phayaob@gmail.com>
2018-12-29 23:20:39 +0700

    Added implementation of Spring Session with Redis

--- src/test/java/com/iphayao/demo/DemoApplicationTests.java
+++ src/test/java/com/iphayao/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.iphayao.springbootsession;
+package com.iphayao.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootSessionApplicationTests {
+public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
