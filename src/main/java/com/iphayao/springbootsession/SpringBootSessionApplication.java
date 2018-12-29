7e3a82e533659cbe3cab2dc6fa60aeb6cb0f86d0
iphayao <phayaob@gmail.com>
2018-12-29 17:39:00 +0700

    Initialize project

---
+++ src/main/java/com/iphayao/springbootsession/SpringBootSessionApplication.java
@@ -0,0 +1,14 @@
+package com.iphayao.springbootsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSessionApplication.class, args);
+	}
+
+}
+
